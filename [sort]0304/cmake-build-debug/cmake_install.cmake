0d423ba5b9076888dba2af52eb23d07b04a26b7c
최영은 <77961304+yech0i@users.noreply.github.com>
2022-03-10 23:46:51 +0900

    modify .gitignore

--- [sort]0304/cmake-build-debug/cmake_install.cmake
+++ [sort]0304/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/CLion/0304
+# Install script for directory: C:/GitHub/Altu-Bitu_ye/[sort]0304
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/CLion/0304/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/GitHub/Altu-Bitu_ye/[sort]0304/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
