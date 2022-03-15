bff5fb1a38238c2ce22ac690882a9e1b62811df0
최영은 <77961304+yech0i@users.noreply.github.com>
2022-03-15 12:37:36 +0900

    2776번 수정

--- [map_set]0308/2776.cpp
+++ [map_set]0308/2776.cpp
@@ -1,53 +1,36 @@
 #include <iostream>
-#include <algorithm>
-//#include <set>
+#include <unordered_set>
 
 using namespace std;
 
 int main() {
 
-    int t;  //테스트케이스 개수
-    scanf("%d", &t);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    int n, m;   //수첩 1 정수 개수, 수첩 2 정수 개수
-    //set<int> note;  //수첩 1
-    int *note;  //수첩 1
-    int num;    //수첩 2에 저장할 정수
+    int t;  //테스트케이스 개수
+    cin >> t;
 
     while (t--) {
 
-        scanf("%d", &n);    //수첩 1 정수 개수 입력
-        note = new int[n];
+        unordered_set<int> note;    //수첩 1
+        int n, m;   //수첩 1 정수 개수, 수첩 2 정수 개수
+        int num;    //수첩에 저장할 정수
+
+        cin >> n;    //수첩 1 정수 개수 입력
 
         for (int i = 0; i < n; i++) {   //수첩 1 정수 입력
-            scanf("%d", &note[i]);
+            cin >> num;
+            note.insert(num);
         }
 
-        sort(note, note + n); //수첩 1 정렬
-
-        scanf("%d", &m);    //수첩 2 정수 개수 입력
+        cin >> m;    //수첩 2 정수 개수 입력
         while (m--) {
-            scanf("%d", &num);
-            printf("%d\n", binary_search(note, note + n, num));
+            cin >> num;
+            if(note.find(num) != note.end()) cout << 1 << '\n';
+            else cout << 0 << '\n';
         }
-
-        //시간 초과
-        /*
-        bool exist;
-        while(m--) {
-            exist = false;
-            scanf("%d", &num);
-            for (auto iter:note) {
-                if (iter == num) {
-                    exist = true;
-                    printf("1\n");   //있으면 1 출력
-                    note.erase(iter);
-                    break;
-                }
-            }
-            if (!exist) printf("0\n");  //없으면 0 출력
-        }
-         */
     }
 
     return 0;
